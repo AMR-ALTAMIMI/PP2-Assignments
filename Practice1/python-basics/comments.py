3f89a1f1fce5b262518f81177fd4c731caf2180e
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 00:56:43 +0500

    Create comments.py
    Add comments examples

---
+++ Practice1/python-basics/comments.py
@@ -0,0 +1,23 @@
+# Task 1: Single Line Comment
+# This is a comment
+print("Hello World")
+
+# Task 2: Inline Comment
+print("Python")  # This is an inline comment
+
+# Task 3: Multi-line Comment
+"""
+This is a
+multi-line comment
+example
+"""
+print("Comments")
+
+# Task 4: Comment About Student
+# Student Name: Amr Al-Tamimi
+print("PP2 Practice")
+
+# Task 5: Explain Code with Comments
+# The variable stores age
+age = 20
+print(age)
