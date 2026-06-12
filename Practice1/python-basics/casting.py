88cff698b70f537f7b3f54f7b89e4f0c070f6c90
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 01:01:32 +0500

    Create casting.py
    Add casting examples

---
+++ Practice1/python-basics/casting.py
@@ -0,0 +1,19 @@
+# Task 1: Cast Float to Integer
+x = int(5.9)
+print(x)
+
+# Task 2: Cast Integer to Float
+y = float(5)
+print(y)
+
+# Task 3: Cast Number to String
+z = str(10)
+print(z)
+
+# Task 4: Convert String to Integer
+a = int("15")
+print(a)
+
+# Task 5: Convert String to Float
+b = float("20")
+print(b)
