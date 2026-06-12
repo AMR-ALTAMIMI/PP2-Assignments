2335dee57709e2ba73c869b7f92b5a9b9b0b90ef
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 00:58:07 +0500

    Create variables.py
    Add variables examples

---
+++ Practice1/python-basics/variables.py
@@ -0,0 +1,23 @@
+# Task 1: Create a Variable
+x = 5
+print(x)
+
+# Task 2: Change Variable Type
+x = "Amr"
+print(x)
+
+# Task 3: Assign Multiple Variables
+a, b, c = "Apple", "Banana", "Cherry"
+print(a, b, c)
+
+# Task 4: One Value to Multiple Variables
+x = y = z = "Orange"
+print(x, y, z)
+
+# Task 5: Global Variable Example
+name = "Amr"
+
+def myfunc():
+    print("Student:", name)
+
+myfunc()
