a4a70b075f59ef3d2ba13f0f06c330bb1bd47772
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 01:02:16 +0500

    Create strings.py
    Add strings examples

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,26 @@
+# Task 1: Loop Through a String
+for x in "Python":
+    print(x)
+
+# Task 2: Find String Length
+text = "Hello World"
+print(len(text))
+
+# Task 3: Check if Word Exists
+txt = "The best things in life are free!"
+print("free" in txt)
+
+# Task 4: String Slicing
+txt = "Hello World"
+print(txt[0:5])
+
+# Task 5: Convert String to Uppercase
+name = "amr"
+print(name.upper())
+
+# Task 6: Convert String to Lowercase
+print(name.lower())
+
+# Task 7: Replace Text
+txt = "Hello World"
+print(txt.replace("World", "Python"))
