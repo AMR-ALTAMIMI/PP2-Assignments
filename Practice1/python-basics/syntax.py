1bbd522c88e2a6a111f92b4db473f98d1d085d5d
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 00:53:26 +0500

    Create syntax.py
    Add syntax examples

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,18 @@
+# Task 1: Print Hello World
+print("Hello World")
+
+# Task 2: Check Python Version
+import sys
+print(sys.version)
+
+# Task 3: Using Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Task 4: Multiple Statements in One Line
+print("Python"); print("Programming")
+
+# Task 5: Simple Arithmetic Operation
+x = 5
+y = 10
+print(x + y)
