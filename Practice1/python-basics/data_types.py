660608fa6821d3aaa60c1ae3ecca1e63920566ed
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 00:59:20 +0500

    Create data_types.py
    Add data types examples

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,19 @@
+# Task 1: String Data Type
+x = "Hello World"
+print(type(x))
+
+# Task 2: Integer Data Type
+x = 20
+print(type(x))
+
+# Task 3: Float Data Type
+x = 20.5
+print(type(x))
+
+# Task 4: Boolean Data Type
+x = True
+print(type(x))
+
+# Task 5: List Data Type
+x = ["apple", "banana", "cherry"]
+print(type(x))
