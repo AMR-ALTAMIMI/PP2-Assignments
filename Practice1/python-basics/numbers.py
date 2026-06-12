5a41685cae7c76d2bead90194dbe8a38909831e3
AMR-ALTAMIMI <amr.khaledabdo90@gmail.com>
2026-06-13 01:00:32 +0500

    Create numbers.py
    Add numbers examples

---
+++ Practice1/python-basics/numbers.py
@@ -0,0 +1,22 @@
+# Task 1: Integer Number
+x = 10
+print(x)
+
+# Task 2: Float Number
+y = 10.5
+print(y)
+
+# Task 3: Complex Number
+z = 3j
+print(z)
+
+# Task 4: Check Number Types
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Task 5: Number Operations
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
