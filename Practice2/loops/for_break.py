3cf6c4c43f7b6f1ffdbce88e015b7c55b22d06cd
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-13 14:40:23 +0500

    Create for_break.py
    Add for_break.py examples

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,47 @@
+# Task 1: Break at Banana
+
+fruits = ["apple", "banana", "cherry"]
+
+for fruit in fruits:
+    if fruit == "banana":
+        break
+
+    print(fruit)
+
+
+# Task 2: Stop at Number 4
+
+for i in range(1, 10):
+    if i == 4:
+        break
+
+    print(i)
+
+
+# Task 3: Break in Names
+
+names = ["Amr", "Ali", "Ahmed"]
+
+for name in names:
+    if name == "Ali":
+        break
+
+    print(name)
+
+
+# Task 4: Break Example
+
+for x in range(5):
+    if x == 2:
+        break
+
+    print(x)
+
+
+# Task 5: Stop Search
+
+for letter in "Python":
+    if letter == "h":
+        break
+
+    print(letter)
