97352c3d439fbe41f6e8ad7c2bfa3d21453d83c7
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-13 14:30:14 +0500

    Create if_else.py
    Add if_else examples

---
+++ Practice2/if_else/if_else.py
@@ -0,0 +1,49 @@
+# Task 1: If Else Statement
+
+age = 16
+
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
+
+
+# Task 2: Check Even or Odd Number
+
+number = 7
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+
+# Task 3: Check Password
+
+password = "1234"
+
+if password == "1234":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+
+# Task 4: Compare Numbers
+
+x = 15
+y = 20
+
+if x > y:
+    print("x is greater")
+else:
+    print("y is greater")
+
+
+# Task 5: Temperature Check
+
+temperature = 30
+
+if temperature > 25:
+    print("Hot Weather")
+else:
+    print("Cold Weather")
