5ae19d644aecb7a52b5ef6090db97af3cb127680
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-13 14:38:00 +0500

    Create while_continue.py
    Add  while_continue.py examples

---
+++ Practice2/loops/while_continue.py
@@ -0,0 +1,63 @@
+# Task 1: Skip Number 3
+
+i = 0
+
+while i < 5:
+    i += 1
+
+    if i == 3:
+        continue
+
+    print(i)
+
+
+# Task 2: Print Odd Numbers
+
+x = 0
+
+while x < 10:
+    x += 1
+
+    if x % 2 == 0:
+        continue
+
+    print(x)
+
+
+# Task 3: Skip Number 5
+
+n = 0
+
+while n < 7:
+    n += 1
+
+    if n == 5:
+        continue
+
+    print(n)
+
+
+# Task 4: Skip Student Number
+
+student = 0
+
+while student < 5:
+    student += 1
+
+    if student == 2:
+        continue
+
+    print(student)
+
+
+# Task 5: Continue Example
+
+a = 0
+
+while a < 4:
+    a += 1
+
+    if a == 1:
+        continue
+
+    print(a)
