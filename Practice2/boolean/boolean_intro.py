88959829b0b5749046695c1f1deb38dc255106f2
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-13 05:44:28 +0500

    Create boolean_intro.py
    Add Boolean Introduction Examples

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,23 @@
+# Task 1: Boolean Values
+print(True)
+print(False)
+
+# Task 2: Boolean Function
+print(bool("Python"))
+print(bool(""))
+print(bool(100))
+
+# Task 3: Check Student Status
+is_student = True
+print(is_student)
+
+# Task 4: Boolean from Comparison
+x = 10
+y = 5
+print(x > y)
+
+# Task 5: Boolean in If Statement
+age = 20
+
+if age >= 18:
+    print("Adult")
