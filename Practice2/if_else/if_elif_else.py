7a5fab6ea90b19bb2ea5bc4854d5e6fc0189f8a7
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-13 14:31:54 +0500

    Create if_elif_else.py
    Add  if_elif_else.py  examples

---
+++ Practice2/if_else/if_elif_else.py
@@ -0,0 +1,58 @@
+# Task 1: Grade Evaluation
+
+grade = 85
+
+if grade >= 90:
+    print("Excellent")
+elif grade >= 75:
+    print("Very Good")
+else:
+    print("Good")
+
+
+# Task 2: Traffic Light System
+
+light = "yellow"
+
+if light == "green":
+    print("Go")
+elif light == "yellow":
+    print("Ready")
+else:
+    print("Stop")
+
+
+# Task 3: Number Check
+
+number = 0
+
+if number > 0:
+    print("Positive")
+elif number < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
+# Task 4: Student Result
+
+score = 45
+
+if score >= 50:
+    print("Passed")
+elif score >= 40:
+    print("Need Improvement")
+else:
+    print("Failed")
+
+
+# Task 5: Weather Advice
+
+temperature = 18
+
+if temperature > 30:
+    print("Very Hot")
+elif temperature > 20:
+    print("Nice Weather")
+else:
+    print("Cold Weather")
