af10c2e39dd985d086508944b2cac8af72501552
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-20 00:37:01 +0500

    Create math.py

---
+++ Practice4/math.py
@@ -0,0 +1,31 @@
+# math.py
+
+import math
+import random
+
+print(min(5, 2, 8))
+print(max(5, 2, 8))
+
+print(abs(-10))
+
+print(round(3.7))
+
+print(pow(2, 3))
+
+print(math.sqrt(25))
+
+print(math.ceil(4.2))
+
+print(math.floor(4.9))
+
+print(random.randint(1, 10))
+
+names = ["Ali", "Ahmed", "Sara"]
+
+print(random.choice(names))
+
+numbers = [1, 2, 3, 4, 5]
+
+random.shuffle(numbers)
+
+print(numbers)
