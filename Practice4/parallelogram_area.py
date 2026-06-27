44ba37c8e1933c2f0885603720f66cb992a1fddc
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-27 16:50:22 +0500

    Create parallelogram_area.py

---
+++ Practice4/parallelogram_area.py
@@ -0,0 +1,10 @@
+# Input values
+base = 5
+height = 6
+
+# Calculate area
+area = base * height
+
+print("Length of base:", base)
+print("Height of parallelogram:", height)
+print("Area of parallelogram:", float(area))
