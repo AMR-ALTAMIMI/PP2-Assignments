c36720100194aa4b0ef4d5fe789ac2f8dfd08669
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-06-27 16:48:43 +0500

    Create . trapezoid_area.py

---
+++ Practice4/. trapezoid_area.py
@@ -0,0 +1,12 @@
+# Input values
+height = 5
+base1 = 5
+base2 = 6
+
+# Calculate area
+area = ((base1 + base2) * height) / 2
+
+print("Height:", height)
+print("Base, first value:", base1)
+print("Base, second value:", base2)
+print("Area of trapezoid:", area)
