28ccbc5d7fcabfe89971097a2ad3b18bd8b20336
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-07-11 10:50:04 +0500

    Update functions.sql

--- TSIS1/functions.sql
+++ TSIS1/functions.sql
@@ -1,55 +1,15 @@
--- =========================================
--- TSIS1 - Search Contacts
--- =========================================
+-- ======================================
+-- TSIS1 Functions
+-- ======================================
 
+-- Search by name, email or any phone number
 CREATE OR REPLACE FUNCTION search_contacts(p_query TEXT)
-RETURNS TABLE (
-    id INT,
-    username VARCHAR,
-    email VARCHAR,
-    birthday DATE,
-    group_name VARCHAR,
-    phone VARCHAR,
-    phone_type VARCHAR
-)
-LANGUAGE plpgsql
-AS $$
-BEGIN
-    RETURN QUERY
-    SELECT
-        c.id,
-        c.username,
-        c.email,
-        c.birthday,
-        g.name,
-        p.phone,
-        p.type
-    FROM contacts c
-    LEFT JOIN groups g
-        ON c.group_id = g.id
-    LEFT JOIN phones p
-        ON c.id = p.contact_id
-    WHERE
-        c.username ILIKE '%' || p_query || '%'
-        OR c.email ILIKE '%' || p_query || '%'
-        OR p.phone ILIKE '%' || p_query || '%';
-END;
-$$;
-
-
--- =========================================
--- Pagination Function
--- =========================================
-
-CREATE OR REPLACE FUNCTION get_contacts_paginated(
-    p_limit INT,
-    p_offset INT
-)
 RETURNS TABLE(
     id INT,
     username VARCHAR,
     email VARCHAR,
-    birthday DATE,
+    phone VARCHAR,
+    phone_type VARCHAR,
     group_name VARCHAR
 )
 LANGUAGE plpgsql
@@ -60,13 +20,17 @@ BEGIN
         c.id,
         c.username,
         c.email,
-        c.birthday,
+        p.phone,
+        p.type,
         g.name
     FROM contacts c
+    LEFT JOIN phones p
+        ON c.id = p.contact_id
     LEFT JOIN groups g
         ON c.group_id = g.id
-    ORDER BY c.username
-    LIMIT p_limit
-    OFFSET p_offset;
+    WHERE
+        c.username ILIKE '%' || p_query || '%'
+        OR c.email ILIKE '%' || p_query || '%'
+        OR p.phone ILIKE '%' || p_query || '%';
 END;
 $$;
