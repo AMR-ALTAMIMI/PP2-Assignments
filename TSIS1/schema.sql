1b747be797d2f9b9932fea700612cdf684038f89
Amr Khaled AL-tamimi <amr.khaledabdo90@gmail.com>
2026-07-11 11:25:42 +0500

    Update schema.sql

--- TSIS1/schema.sql
+++ TSIS1/schema.sql
@@ -1,7 +1,4 @@
--- =========================
--- TSIS1 Extended PhoneBook
--- =========================
-
+-- Drop old tables if they exist
 DROP TABLE IF EXISTS phones CASCADE;
 DROP TABLE IF EXISTS contacts CASCADE;
 DROP TABLE IF EXISTS groups CASCADE;
@@ -15,20 +12,20 @@ CREATE TABLE groups (
 -- Contacts table
 CREATE TABLE contacts (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(100) UNIQUE NOT NULL,
+    name VARCHAR(100) UNIQUE NOT NULL,
     email VARCHAR(100),
     birthday DATE,
     group_id INTEGER REFERENCES groups(id),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Phones table
+-- Phones table (one contact can have many phones)
 CREATE TABLE phones (
     id SERIAL PRIMARY KEY,
     contact_id INTEGER REFERENCES contacts(id) ON DELETE CASCADE,
     phone VARCHAR(20) NOT NULL,
     type VARCHAR(10)
-        CHECK(type IN ('home','work','mobile'))
+        CHECK (type IN ('home','work','mobile'))
 );
 
 -- Default groups
@@ -37,5 +34,4 @@ VALUES
 ('Family'),
 ('Friend'),
 ('Work'),
-('Other')
-ON CONFLICT(name) DO NOTHING;
+('Other');
